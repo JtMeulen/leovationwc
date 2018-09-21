715b73e4f5884f38d7bd0d5f41287e1f97a4ec92
Claudiu Dragan <dragancla@gmail.com>
2018-09-21 13:19:10 +0200

    header - initial commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
-
+import leologo from '../../logo_banner.png';
 import './Header.css';
-
 const Header = () => {
     return(
         <div className="header">
-            
+                    <img className="logoheader" src={leologo} />
         </div>
     )
 }
-
 export default Header;
